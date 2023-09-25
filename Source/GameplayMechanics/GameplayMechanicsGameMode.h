6f84febe3468f96c7917d2159fe3d752a7b1f44a
Kyrian <kyrian@example.com>
2023-09-25 16:18:25 +0100

    Initial commit

---
+++ Source/GameplayMechanics/GameplayMechanicsGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GameplayMechanicsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AGameplayMechanicsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGameplayMechanicsGameMode();
+};
+
+
+
